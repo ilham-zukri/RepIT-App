3f9d08b5dddf1a9cd23a7da40600f8f883112dd2
ilham-zukri <zukri.ilham@gmail.com>
2023-03-03 01:42:48 +0700

    Interface done, action hasn't added

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,48 +1,186 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  LoginPage({super.key});
 
+  final TextEditingController userNameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            width: size.width,
-            height: size.height / 3.5,
-            decoration: const BoxDecoration(
-              color: Color(0xff00ABB3),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(80),
-                bottomRight: Radius.circular(80),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              width: size.width,
+              height: size.height / 3.5,
+              decoration: const BoxDecoration(
+                color: Color(0xff00ABB3),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(80),
+                  bottomRight: Radius.circular(80),
+                ),
               ),
             ),
-          ),
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: size.height / 3.5 - 75),
-                    child: Card(
-                      elevation: 7,
-                      shape: const CircleBorder(),
-                      child: CircleAvatar(
-                        radius: 75,
-                        backgroundColor: Colors.transparent,
-                        child: SvgPicture.asset("assets/logos/Rlogo.svg", fit: BoxFit.contain,),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: size.height / 3.5 - 75),
+                      child: Card(
+                        elevation: 5,
+                        shape: const CircleBorder(),
+                        child: CircleAvatar(
+                          radius: 75,
+                          backgroundColor: Colors.transparent,
+                          child: SvgPicture.asset(
+                            "assets/logos/Rlogo.svg",
+                            fit: BoxFit.contain,
+                          ),
+                        ),
                       ),
                     ),
+                  ],
+                ),
+
+                ////SUBMITION FORM////
+                Container(
+                  margin: const EdgeInsets.only(top: 24, right: 24, left: 24),
+                  child: Column(
+                    children: [
+                      //// Username Field ////
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          "Username",
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 8),
+                        height: 41,
+                        child: TextField(
+                          controller: widget.userNameController,
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.all(10),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      //// Password Field ////
+                      Container(
+                        margin: const EdgeInsets.only(top: 14),
+                        child: const Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            "Password",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 8),
+                        height: 41,
+                        child: TextField(
+                          controller: widget.passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.all(10),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      //// Login Button ////
+                      Container(
+                        margin: const EdgeInsets.only(top: 35),
+                        height: 41,
+                        width: size.width,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xff00ABB3),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50)),
+                              elevation: 5),
+                          onPressed: () {},
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 12),
+                        child: RichText(
+                          text: TextSpan(
+                            children: <TextSpan>[
+                              const TextSpan(
+                                  text: "Belum memiliki akun? ",
+                                  style: TextStyle(color: Colors.black)),
+                              TextSpan(
+                                  text: "Hubungi Administrator",
+                                  style:
+                                      const TextStyle(color: Color(0xff00C2FF)),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {})
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ],
-          )
-        ],
+                ),
+                
+              ],
+            ),
+            SizedBox(
+              height: size.height,
+              width: size.width,
+              child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: size.height/30),
+                        height: 43,
+                        width: 107,
+                        child: SvgPicture.asset(
+                          "assets/logos/mainlogo.svg",
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ],
+                  ),
+            ),
+          ],
+        ),
       ),
     );
   }
