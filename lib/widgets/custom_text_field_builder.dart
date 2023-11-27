669bd8d0162bcdfde601927541c57824c7f0dbf6
ilham-zukri <zukri.ilham@gmail.com>
2023-11-28 00:34:52 +0700

    feat(TextBuilder): adding enable property

--- lib/widgets/custom_text_field_builder.dart
+++ lib/widgets/custom_text_field_builder.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-Widget regularTextFieldBuilder({required String labelText, required TextEditingController controller, required bool obscureText}) {
+Widget regularTextFieldBuilder({required String labelText, required TextEditingController controller, required bool obscureText, String? hintText, bool enabled = true}) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
@@ -15,9 +15,11 @@ Widget regularTextFieldBuilder({required String labelText, required TextEditingC
         margin: const EdgeInsets.only(top: 8),
         height: 41,
         child: TextField(
+          enabled: enabled,
           controller: controller,
           obscureText: obscureText,
           decoration: InputDecoration(
+            hintText: hintText,
             contentPadding: const EdgeInsets.all(10),
             filled: true,
             fillColor: Colors.white,
@@ -31,7 +33,7 @@ Widget regularTextFieldBuilder({required String labelText, required TextEditingC
   );
 }
 
-Widget descriptionTextFieldBuilder({required String labelText, required TextEditingController controller}) {
+Widget descriptionTextFieldBuilder({required String labelText, required TextEditingController controller, String? hintText, bool enabled = true}) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
@@ -46,9 +48,11 @@ Widget descriptionTextFieldBuilder({required String labelText, required TextEdit
         margin: const EdgeInsets.only(top: 8),
         height: 112,
         child: TextField(
+          enabled: enabled,
           maxLines: 100,
           controller: controller,
           decoration: InputDecoration(
+            hintText: hintText,
             contentPadding: const EdgeInsets.all(10),
             filled: true,
             fillColor: Colors.white,
